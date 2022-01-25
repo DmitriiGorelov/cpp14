a18dc62e661b32af1f5e54e3e7f970d976ab1164
DmitriiGorelov <dmitry.gorelov@gmail.com>
2022-01-25 22:38:35 +0100

    new projects

--- C14/C14.cpp
+++ C14/C14.cpp
@@ -1,11 +1,38 @@
-// C14.cpp : Defines the entry point for the console application.
-//
+#include <algorithm>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <tuple>
+#include <vector>
 
-#include "stdafx.h"
+//enum Outer { a = 1, b = 2, c = 3 };
+//template <typename>
+//struct S { enum Inner { a = 100, c = 102 }; };
+//
+//template <int>
+//struct G{ typedef int b; };
 
 
 int main()
-{
-    return 0;
-}
+{    
+    /*int i = S<G< 5 >> ::c > ::b > ::a;
+    i = ::c > ::b >= ::a;*/
 
+    std::vector<std::string> v = { "This", "is", "an", "example" };
+
+    std::string* p;
+    std::size_t sz;
+    std::tie(p, sz) = std::get_temporary_buffer<std::string>(v.size());
+    sz = std::min(sz, v.size());
+
+    std::uninitialized_copy_n(v.begin(), sz, p);
+
+    for (std::string* i = p; i != p + sz; ++i) {
+        std::cout << *i << ' ';
+        i->~basic_string<char>();
+    }
+    std::return_temporary_buffer(p);
+
+
+    
+}
